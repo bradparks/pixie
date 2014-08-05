5c091f8e02a7d12bc7fa945eae772856ad2b6185
Nathan Dinsmore <nfdins@gmail.com>
2014-08-05 17:54:06 -0400

    Add Chrome app manifest and background page

---
+++ background.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('index.html', {
+      bounds: {
+        width: 960,
+        height: 600
+      },
+      state: 'maximized'
+    });
+  });
+
+}());
